7e026186bc3b15b41c27e33335a8c1e50fc9f874
Stefan Henrique Frade <stefan.henrique@outlook.com>
2019-11-27 08:13:19 -0300

    Iniciando OO

---
+++ oo.py
@@ -0,0 +1,6 @@
+'''
+Orientação a objeto
+Crie uma classe contendo 3 propriedades com seus respectivos gets e sets e um método que multiplique as 3 retornando o produto. Deixe um exemplo de utilização da sua classe no final do código.
+'''
+
+
