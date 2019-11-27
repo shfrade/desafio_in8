c950ace78c3550842685198761fec6b0485e664d
Stefan Henrique Frade <stefan.henrique@outlook.com>
2019-11-27 08:23:18 -0300

    Finalizando OO

--- oo.py
+++ oo.py
@@ -4,3 +4,47 @@ Crie uma classe contendo 3 propriedades com seus respectivos gets e sets e um m�
 '''
 
 
+class Exemplo:
+    def __init__(self):
+        # iniciando em 1 para não dar erro na multiplicação caso passe menos de 3 números.
+        self.__atributo1 = 1
+        self.__atributo2 = 1
+        self.__atributo3 = 1
+
+    def produto(self):
+        return self.getAtributo1() * self.getAtributo2() * self.getAtributo3()
+
+    def getAtributo1(self):
+        return self.__atributo1
+
+    def setAtributo1(self, num=None):
+        if not isinstance(num, (int, float)):
+            return False
+        self.__atributo1 = num
+        return True
+
+    def getAtributo2(self):
+        return self.__atributo2
+
+    def setAtributo2(self, num=None):
+        if not isinstance(num, (int, float)):
+            return False
+        self.__atributo2 = num
+        return True
+
+    def getAtributo3(self):
+        return self.__atributo3
+
+    def setAtributo3(self, num=None):
+        if not isinstance(num, (int, float)):
+            return False
+        self.__atributo3 = num
+        return True
+
+
+### Exemplo de uso abaixo
+exemplo = Exemplo()
+exemplo.setAtributo1(5)
+exemplo.setAtributo2(7)
+exemplo.setAtributo3(10)
+print(f"O produto de {exemplo.getAtributo1()}*{exemplo.getAtributo2()}*{exemplo.getAtributo3()} é: {exemplo.produto()}")
