9a71b0304cebd116b15c9ec402e7502875493ed3
Stefan Henrique Frade <stefan.henrique@outlook.com>
2019-11-27 07:51:09 -0300

    Iniciando array

---
+++ arrays.py
@@ -0,0 +1,5 @@
+'''
+Tendo os arrays ['ES', 'MG', 'RJ', 'SP'] e ['São Paulo', 'Rio de Janeiro', 'Minas Gerais', 'Espírito Santo'], percorra os vetores dados e crie um outro com a seguinte estrutura: ['ES'=>'Espírito Santo', 'MG'=>'Minas Gerais', 'RJ'=>'Rio de Janeiro', 'SP'=>'São Paulo']. Depois de criado terceiro vetor, leia-o e imprima em cada linha a chave de cada posição e seu respectivo valor separados por "-".
+'''
+uf_sigla = ['ES', 'MG', 'RJ', 'SP']
+uf_cidade = ['São Paulo', 'Rio de Janeiro', 'Minas Gerais', 'Espírito Santo']
