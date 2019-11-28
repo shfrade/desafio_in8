096d3461c311cce8c78f65940111a9b850194aaa
Stefan Henrique Frade <stefan.henrique@outlook.com>
2019-11-28 12:39:01 -0300

    Básico da model realizada

--- api/biblioteca/views.py
+++ api/biblioteca/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
+
 
 # Create your views here.
+class AutorViewSet(viewsets.ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+
+
+class LivroViewSet(viewsets.ModelViewSet):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+
+
+class AlunoViewSet(viewsets.ModelViewSet):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+
+class EmprestimoViewSet(viewsets.ModelViewSet):
+
+    queryset = Emprestimo.objects.all()
+    serializer_class = EmprestimoSerializer
